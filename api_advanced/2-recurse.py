f45e30757e8842f406080181c9f090158e62b551
ynthia16 <c.umugwanez@alustudent.com>
2023-07-15 20:40:08 +0000

    third file

--- api_advanced/2-recurse.py
+++ api_advanced/2-recurse.py
@@ -1,44 +1,31 @@
 #!/usr/bin/python3
-"""
-Function that queries the Reddit API and prints
-the top ten hot posts of a subreddit
-"""
+"""Contains recurse function"""
 import requests
-import sys
 
 
-def add_title(hot_list, hot_posts):
-    if len(hot_posts) == 0:
-        return
-    hot_list.append(hot_posts[0]['data']['title'])
-    hot_posts.pop(0)
-    add_title(hot_list, hot_posts)
-
-
-def recurse(subreddit, hot_list=[], after=None):
-    agent = 'Mozilla/5.0'
+def recurse(subreddit, hot_list=[], after="", count=0):
+    """Returns a list of titles of all hot posts on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
     headers = {
-        'User-Agent': agent
+        "User-Agent": "0x16-api_advanced:project:\
+v1.0.0 (by /u/firdaus_cartoon_jr)"
     }
-
     params = {
-        'after': after
+        "after": after,
+        "count": count,
+        "limit": 100
     }
-
-    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    response = requests.get(url,
-                            headers=headers,
-                            params=params,
+    response = requests.get(url, headers=headers, params=params,
                             allow_redirects=False)
-
-    if response.status_code != 200:
+    if response.status_code == 404:
         return None
 
-    reddit = response.json()
-    hot_posts = reddit['data']['children']
-    add_title(hot_list, hot_posts)
-    after = reddit['data']['after']
-    if not after:
-        return hot_list
-    else:
-        return recurse(subreddit, hot_list=hot_list, after=after)
+    results = response.json().get("data")
+    after = results.get("after")
+    count += results.get("dist")
+    for c in results.get("children"):
+        hot_list.append(c.get("data").get("title"))
+
+    if after is not None:
+        return recurse(subreddit, hot_list, after, count)
+    return hot_list
